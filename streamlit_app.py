1ee9b258cd7ccd3f605587ff841d7ebe2c651557
ikapote <113245224+ikapote@users.noreply.github.com>
2023-11-29 10:27:55 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,9 @@ streamlit.text(' 🐔 Hard-boiled Free-Ranged Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
                
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
