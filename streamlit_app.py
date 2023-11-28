89415c191589f27dd5fced72337c8e8ad0879acd
ikapote <113245224+ikapote@users.noreply.github.com>
2023-11-29 10:12:23 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 
 streamlit.title('My parents New Healthy Dinner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal)
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-Ranged Egg')
+               
